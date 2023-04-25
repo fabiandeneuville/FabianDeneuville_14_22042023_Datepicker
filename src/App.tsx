cefa34bbe80adb0401c2c8669d9dd3bee04c1012
Fabian Deneuville <deneuville.fabian@live.fr>
2023-04-25 23:00:26 +0200

    Removing global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import GlobalStyle from "./globalStyle";
 import DatePicker from "./components/DatePicker/DatePicker";
 
 function App() {
   return (
     <>
-      <GlobalStyle/>
       <DatePicker
       required={true}
       label="Start Date (yyyy-mm-dd)"
